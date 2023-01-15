a0081477a65f77edc4c634e6316233c3765946a2
denisk13005 <kirmanndenis@gmail.com>
2023-01-15 10:39:02 +0100

    objet en props

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <template>
   <div id="app">
-    App page ddd
+    choosen option :{{chooseOption}} <br/>
+    id:{{ optionId }}
    <Paginate-Tab :array="list"/>
-   <EditableSelect :options="optionsList"/>
+   <EditableSelect :options="optionsList" @choosenOption="choosenOption"/>
   </div>
 </template>
 
@@ -18,9 +19,19 @@ export default {
   data() {
     return {
       list: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22],
-      optionsList : ["france", "danemark","italie","espagne","canada","senegal"]
+      optionsList : [{value : 'france', id: 1},{value:"italie",id:2}],
+      chooseOption : '',
+      optionId : ''
      
     }
   },
+  methods : {
+    choosenOption (payload){
+      this.chooseOption = payload.value
+      this.optionId = payload.id
+      console.log(payload.value)
+      console.log(payload.id)
+    }
+  }
 }
 </script>
--- src/components/EditableSelect.vue
+++ src/components/EditableSelect.vue
@@ -8,7 +8,7 @@
     @keyup="filterOptions"
     @click="edit"/>
     <div v-show="showOptions" class="optionsContainer">
-      <div class="option" v-for="(el,index) in getOptions" :key="index" @click="chooseOption" > {{ el }} </div>
+      <div class="option" v-for="(el,index) in getOptions" :key="index" @click="chooseOption(el)" > {{ el.value }}, {{ el.id }} </div>
     </div>
   </div>
 </template>
@@ -33,20 +33,20 @@
         }
       },
       methods : {
-        edit(e){
+        edit(){
           this.showOptions = !this.showOptions
-          console.log(e)
         },
-        chooseOption(e){
-          let value = e.target.innerText
-          this.optionValue = value
+        chooseOption(el){
+          console.log(el)         
+          this.optionValue = el.value          
           this.showOptions = false
+          this.$emit('choosenOption', el)
         },
         filterOptions(e){
           this.showOptions = true
           let search = e.target.value
           console.log(search)
-           this.optionsToShow =  this.options.filter(el => el.includes(search))
+           this.optionsToShow =  this.options.filter(el => el.value.includes(search))
         }
       },
       computed:{
